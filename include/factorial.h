d65eb3ce894c03b2e7f3e6fc0e065f2b6e12ec99
David Durst <davidbdurst@gmail.com>
2020-11-22 16:36:20 -0800

    removed factorial starter code

--- include/factorial.h
+++
@@ -1,4 +0,0 @@
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
-unsigned int factorial(unsigned int number);
-#endif 
--- src/cpp/lib/factorial.cpp
+++
@@ -1,3 +0,0 @@
-unsigned int factorial(unsigned int number) {
-    return number <= 1 ? 1 : factorial(number-1)*number;
-}
