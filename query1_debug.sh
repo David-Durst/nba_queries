b5fa85e0c3ee5fd9bfe1e13ce326b86d325c0eb8
David Durst <davidbdurst@gmail.com>
2020-12-10 13:36:31 -0800

    updated running scripts for query parameter

---
+++ query1_debug.sh
@@ -0,0 +1,5 @@
+mkdir -p build
+cd build
+cmake ..
+make -j4
+./nba_queries /home/durst/big_dev/nba-movement-data/data/csv/shortened_game.csv /home/durst/big_dev/nba-movement-data/data/shots/shots_fixed.csv 1
--- run_main.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build
-cd build
-cmake ..
-make -j4
-./nba_queries /home/durst/big_dev/nba-movement-data/data/csv/default_game.csv /home/durst/big_dev/nba-movement-data/data/shots/shots_fixed.csv
