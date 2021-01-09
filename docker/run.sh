1000c11643467dbd52e6b5baaead3a605e381734
David Durst <davidbdurst@gmail.com>
2021-01-09 10:48:14 -0800

    setting up conda and loading q file

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,5 @@
-docker run --name durst_st_db --rm -dit durst/space-time-db:0.1 bash
+docker run \
+    --name durst_st_db \
+    --rm -dit \
+    --mount type=bind,source="$(pwd)"/q,target=/home/durst/q \
+    durst/space-time-db:0.1 bash
