678c848fc9e8a8b12204434f9f4cb070536f0947
David Durst <davidbdurst@gmail.com>
2021-01-09 11:32:48 -0800

    fixed cmake install, loading data

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,5 @@ docker run \
     --name durst_st_db \
     --rm -dit \
     --mount type=bind,source="$(pwd)"/q,target=/home/durst/q \
+    --mount type=bind,source="$(pwd)"/data,target=/home/durst/big_dev/nba-movement-data/data \
     durst/space-time-db:0.1 bash
