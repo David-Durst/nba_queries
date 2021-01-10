547392d9cf6a38424e4c0e677762d3bdab7ee41e
David Durst <davidbdurst@gmail.com>
2021-01-10 09:29:56 -0800

    added scripts for running cleaned queries in dev or measure mode

---
+++ cleaned_queries_measure.sh
@@ -0,0 +1,5 @@
+mkdir -p build
+cd build
+cmake ..
+make -j4
+./cleaned_nba_queries /home/durst/big_dev/nba-movement-data/data/csv/cleaned_default_game.csv /home/durst/big_dev/nba-movement-data/data/shots/shots_fixed.csv measure
