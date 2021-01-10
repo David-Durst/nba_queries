f94aa645cd65046df43ff9392cb2b97d7f34d07b
David Durst <davidbdurst@gmail.com>
2021-01-09 17:34:48 -0800

    back to loading clean as vectors, then convert to efficient format

--- src/cpp/bin/cleaned_query_main.cpp
+++ src/cpp/bin/cleaned_query_main.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
-#include <fstream>
 #include <vector>
+#include <fstream>
 #include <string>
 #include <thread>
 #include <chrono>
@@ -18,44 +18,44 @@
 using std::string;
 
 int main(int argc, char * argv[]) {
-    cleaned_moment * moments;
-    shot * shots;
-    vector<shot> shots_vec;
-    list<shot_and_player_data> shots_and_players;
-    vector<shot_and_player_data> shots_and_players_vec;
+    vector<cleaned_moment> moments;
+    vector<shot> shots;
+    shot * shots_arr;
+    vector<shot_and_player_data> shots_and_players;
+    list<shot_and_player_data> shots_and_players_list;
+    vector<trajectory_data> trajectories;
     list<trajectory_data> trajectories;
-    vector<trajectory_data> trajectories_vec;
     if (argc != 3) {
         std::cout << "please call this code with 2 arguments: " << std::endl;
         std::cout << "1. path/to/cleaned_moments_file.csv " << std::endl;
         std::cout << "2. path/to/shots_file.csv " << std::endl;
     }
-    string moments_filepath = argv[1], shots_filepath = argv[2];
-    std::fstream shots_file;
+    string moments_file_path = argv[1], shots_file_path = argv[2];
+    std::fstream moments_file, shots_file;
 
     // load the cleaned moments
-    std::cout << "loading cleaned moments file: " << moments_filepath << std::endl;
-    size_t moments_size = load_cleaned_moment_rows(moments_filepath, &moments);
-    std::cout << "moments size: " << moments_size << std::endl;
+    std::cout << "loading cleaned moments file: " << moments_file_path << std::endl;
+    moments_file.open(moments_file_path);
+    load_cleaned_moment_rows_vec(moments_file, moments);
+    moments_file.close();
+    std::cout << "moments size: " << moments.size() << std::endl;
     // load the shots
-    std::cout << "loading shots file: " << shots_filepath << std::endl;
-    shots_file.open(shots_filepath);
-    load_shot_rows_vec(shots_file, shots_vec);
+    std::cout << "loading shots file: " << shots_file_path << std::endl;
+    shots_file.open(shots_file_path);
+    load_shot_rows_vec(shots_file, shots);
     shots_file.close();
-    std::cout << "shots size: " << shots_vec.size() << std::endl;
+    std::cout << "shots size: " << shots.size() << std::endl;
     // filter shots, sort by quarter than gametime
-    long int target_game_id = moments[0].game_id;
+    long int target_game_id = moments.at(0).game_id;
     std::cout << "filtering shots: " << std::endl;
-    shots_vec.erase(std::remove_if(shots_vec.begin(), shots_vec.end(), [target_game_id](shot s){
+    shots.erase(std::remove_if(shots.begin(), shots.end(), [target_game_id](shot s){
         return s.game_id != target_game_id;
-    }), shots_vec.end());
-    std::cout << "new shots size: " << shots_vec.size() << ", sorting shots" << std::endl;
-    std::sort(shots_vec.begin(), shots_vec.end(), [](shot s0, shot s1) {
+    }), shots.end());
+    std::cout << "new shots size: " << shots.size() << ", sorting shots" << std::endl;
+    std::sort(shots.begin(), shots.end(), [](shot s0, shot s1) {
         return (s0.period < s1.period || (s0.period == s1.period && s0.shot_time > s1.shot_time) ||
                 (s0.period == s1.period && s0.shot_time == s1.shot_time && s0.game_event_id < s1.game_event_id));
     });
-    shots = new shot[shots_vec.size()];
-    std::copy(shots_vec.begin(), shots_vec.end(), shots);
 
     std::cout << "running query 3 cleaned" << std::endl;
     coordinate_range origin{{70.0f,16.0f,0}, {90.0f,32.0f, 0}};
