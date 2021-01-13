71d4faf5e16187745e0aad15803140b33e1173d5
David Durst <davidbdurst@gmail.com>
2021-01-13 11:47:49 -0800

    moved debug/release flag to runner script

--- cleaned_queries_debug.sh
+++ cleaned_queries_debug.sh
@@ -1,5 +1,5 @@
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j4
 ./cleaned_nba_queries /home/durst/big_dev/nba-movement-data/data/csv/cleaned_default_game.csv /home/durst/big_dev/nba-movement-data/data/shots/cleaned_shots_fixed.csv debug $(dirname $(pwd))/timing.csv
--- cleaned_queries_measure.sh
+++ cleaned_queries_measure.sh
@@ -1,5 +1,5 @@
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j4
 ./cleaned_nba_queries /home/durst/big_dev/nba-movement-data/data/csv/cleaned_default_game.csv /home/durst/big_dev/nba-movement-data/data/shots/cleaned_shots_fixed.csv measure $(dirname $(pwd))/timing.csv
