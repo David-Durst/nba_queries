56bea5b1bc7f24c8650edf29a144da8839554f24
David Durst <davidbdurst@gmail.com>
2021-01-22 11:45:47 -0800

    adding valgrind, enabling ptrace

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,5 @@ docker run \
     --rm -dit \
     --mount type=bind,source="$(pwd)"/q,target=/home/durst/q \
     --mount type=bind,source="$(pwd)"/data,target=/home/durst/big_dev/nba-movement-data/data \
+    --cap-add=SYS_PTRACE
     durst/space-time-db:0.1 bash
