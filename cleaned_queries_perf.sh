eeb30811fb64ae6f64525cb05e72c70b8bdeed8a
David Durst <davidbdurst@gmail.com>
2021-01-23 14:41:08 -0800

    fixing perf calls

--- cleaned_queries_perf.sh
+++ cleaned_queries_perf.sh
@@ -1,5 +1,6 @@
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j4
-perf record -g ./cleaned_nba_queries /home/durst/big_dev/nba-movement-data/data/csv/cleaned_all_games.csv /home/durst/big_dev/nba-movement-data/data/shots/all_cleaned_shots_fixed.csv measure /home/durst/big_dev/nba-movement-data/data/csv/all_extra_game_data.csv  $(dirname $(pwd))/timing.csv
+perf record --call-graph dwarf ./cleaned_nba_queries /home/durst/big_dev/nba-movement-data/data/csv/cleaned_all_games.csv /home/durst/big_dev/nba-movement-data/data/shots/all_cleaned_shots_fixed.csv measure /home/durst/big_dev/nba-movement-data/data/csv/all_extra_game_data.csv  $(dirname $(pwd))/timing.csv
+perf report --hierarchy
