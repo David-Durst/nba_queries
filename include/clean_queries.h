0fc24d6cbc43df1fee3cbd08a596c3415a197e47
David Durst <davidbdurst@gmail.com>
2021-01-27 20:08:22 -0800

    trying forcing inline

--- include/clean_queries.h
+++ include/clean_queries.h
@@ -104,19 +104,19 @@ void get_players_in_paint_shot_clock_binned(moment_col_store * moments, court_bi
                                             vector<players_in_paint_at_time>& players_in_paint,
                                             coordinate_range paint0, coordinate_range paint1, double end_time);
 
-inline bool point_intersect_no_time(coordinate_range * r, double x_loc, double y_loc) {
+static inline __attribute__((always_inline)) bool point_intersect_no_time(coordinate_range * r, double x_loc, double y_loc) {
     bool x_intersects = x_loc >= r->start.x && x_loc <= r->end.x;
     bool y_intersects = y_loc >= r->start.y && y_loc <= r->end.y;
     return x_intersects && y_intersects;
 }
 
-inline bool point_intersect_no_time_no_ptr(coordinate_range r, double x_loc, double y_loc) {
+static inline __attribute__((always_inline)) bool point_intersect_no_time_no_ptr(coordinate_range r, double x_loc, double y_loc) {
     bool x_intersects = x_loc >= r.start.x && x_loc <= r.end.x;
     bool y_intersects = y_loc >= r.start.y && y_loc <= r.end.y;
     return x_intersects && y_intersects;
 }
 
-inline bool point_intersect_no_time(const coordinate_range& r, const player_data& c) {
+static inline __attribute__((always_inline)) bool point_intersect_no_time(const coordinate_range& r, const player_data& c) {
     bool x_intersects = c.x_loc >= r.start.x && c.x_loc <= r.end.x;
     bool y_intersects = c.y_loc >= r.start.y && c.y_loc <= r.end.y;
     return x_intersects && y_intersects;
