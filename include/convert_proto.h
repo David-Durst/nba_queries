e4bd4fc1606883bc2a61341e6346bed9ab3f5eee
David Durst <davidbdurst@gmail.com>
2021-02-04 15:19:33 -0800

    added moment, moved proto to library

---
+++ include/convert_proto.h
@@ -0,0 +1,7 @@
+#ifndef NBA_QUERIES_CONVERT_PROTO_H
+#define NBA_QUERIES_CONVERT_PROTO_H
+
+#include "moment.pb.h"
+
+
+#endif //NBA_QUERIES_CONVERT_PROTO_H
--- src/cpp/bin/generate_nba_proto_main.cpp
+++ src/cpp/bin/generate_nba_proto_main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "load_data.h"
 #include "query_structs.h"
-#include "addressbook.pb.h"
+#include "moment.pb.h"
 
 int main(int argc, char * argv[]) {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    vector<cleaned_moment> moments;
+    if (argc != 3) {
+        std::cout << "please call this code with 2 arguments: " << std::endl;
+        std::cout << "1. path/to/cleaned_moments_file.csv " << std::endl;
+        std::cout << "5. path/to/output/cleaned/moments.pb" << std::endl;
+        return 0;
+    }
+    string moments_file_path = argv[1], moments_proto_path = argv[2];
+    std::fstream moments_file, proto_file;
+
+    // load the cleaned moments
+    std::cout << "loading cleaned moments file: " << moments_file_path << std::endl;
+    moments_file.open(moments_file_path);
+    load_cleaned_moment_rows_vec(moments_file, moments);
+    moments_file.close();
+    std::cout << "moments size: " << moments.size() << std::endl;
     return 0;
 }
