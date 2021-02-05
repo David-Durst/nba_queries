aa4e03818ca933c7a68966b4c4807460759e191e
David Durst <davidbdurst@gmail.com>
2021-02-04 21:04:44 -0800

    added timing and all games

--- src/cpp/bin/generate_nba_proto_main.cpp
+++ src/cpp/bin/generate_nba_proto_main.cpp
@@ -4,6 +4,7 @@
 #include "query_structs.h"
 #include "convert_proto.h"
 #include "moment.pb.h"
+#include "benchmark.h"
 
 
 int main(int argc, char * argv[]) {
@@ -22,15 +23,21 @@ int main(int argc, char * argv[]) {
 
     // load the cleaned moments
     std::cout << "loading cleaned moments file: " << moments_file_path << std::endl;
+    auto start_internal = Halide::Tools::benchmark_now();
     moments_file.open(moments_file_path);
     load_cleaned_moment_rows_vec(moments_file, moments);
     moments_file.close();
+    auto time_internal = Halide::Tools::benchmark_duration_seconds(start_internal, Halide::Tools::benchmark_now());
+    std::cout << "it took " << time_internal << " seconds to load the data from csv" << std::endl;
     std::cout << "moments size: " << moments.size() << std::endl;
     moments_memory_to_proto(moments, proto_moments);
     std::cout << "proto_moments size: " << proto_moments.elements_size() << std::endl;
+    auto start_proto = Halide::Tools::benchmark_now();
     proto_file.open(proto_file_path);
     proto_moments.SerializeToOstream(&proto_file);
     proto_file.close();
+    auto time_proto = Halide::Tools::benchmark_duration_seconds(start_internal, Halide::Tools::benchmark_now());
+    std::cout << "it took " << time_internal << " seconds to write the data in protobuf" << std::endl;
 
     return 0;
 }
