8ce9f2e32bc574725b058e25d40d66c3bf1d6bb8
David Durst <davidbdurst@gmail.com>
2021-02-06 13:03:42 -0800

    basic c++ benchmark

--- src/bench_cpp/main.cpp
+++ src/bench_cpp/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <vector>
+#include "benchmark.h"
+using std::vector;
 
 int main(int argc, char ** argv) {
+
+    auto start_array_fill = Halide::Tools::benchmark_now();
+    int * arr = new int[7821357];
+    for (int i = 0; i < 7821357; i++) {
+        arr[i] = i;
+    }
+    auto time_array_fill = Halide::Tools::benchmark_duration_seconds(start_array_fill, Halide::Tools::benchmark_now());
+    std::cout << "it took " << time_array_fill * 1e3 << "ms to array_fill" << std::endl;
+
+    auto vec = vector<int>();
+    auto start_vec_fill = Halide::Tools::benchmark_now();
+    vec.resize(7821357);
+    for (int i = 0; i < 7821357; i++) {
+        vec[i] = i;
+    }
+    auto time_vec_fill = Halide::Tools::benchmark_duration_seconds(start_vec_fill, Halide::Tools::benchmark_now());
+    std::cout << "it took " << time_vec_fill * 1e3 << "ms to vec_fill" << std::endl;
+
     return 0;
 }
