77225353f9043ec6e382fe48700deff80cf5acda
David Durst <davidbdurst@gmail.com>
2021-02-06 13:12:59 -0800

    adding push timing

--- src/bench_cpp/main.cpp
+++ src/bench_cpp/main.cpp
@@ -23,5 +23,13 @@ int main(int argc, char ** argv) {
     auto time_vec_alloc = Halide::Tools::benchmark_duration_seconds(start_vec_alloc, Halide::Tools::benchmark_now());
     std::cout << "it took " << time_vec_alloc * 1e3 << "ms to vec_alloc" << std::endl;
 
+    auto start_vec_push = Halide::Tools::benchmark_now();
+    auto vec = vector<int>();
+    for (int i = 0; i < 7821357; i++) {
+        vec.push_back(i);
+    }
+    auto time_vec_push = Halide::Tools::benchmark_duration_seconds(start_vec_push, Halide::Tools::benchmark_now());
+    std::cout << "it took " << time_vec_push * 1e3 << "ms to vec_push" << std::endl;
+
     return 0;
 }
