291c70df5b807e4c6b05103b0d8344d1b346b693
David Durst <davidbdurst@gmail.com>
2021-02-09 14:07:56 -0800

    loading more data

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,7 @@ docker run \
     --rm -dit \
     --mount type=bind,source="$(pwd)"/q,target=/home/durst/q \
     --mount type=bind,source="$(pwd)"/data,target=/home/durst/big_dev/nba-movement-data/data \
+    --mount type=bind,source=/home/durst/big_dev/fiodar_data/2_6_21,target=/home/durst/big_dev/fiodar_data/2_6_21 \
     --cap-add=SYS_PTRACE \
     --cap-add=SYS_ADMIN \
     durst/space-time-db:0.1 bash
