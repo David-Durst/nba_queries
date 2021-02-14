e730c2b20473a8ecbbc67d20e22dac275ab08eeb
David Durst <davidbdurst@gmail.com>
2021-02-14 13:13:54 -0800

    mounting samples in docker start

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,7 @@
 docker run \
     --name durst_st_db_${USER} \
     --rm -dit \
+    --mount type=bind,source="$(pwd)"/samples,target=/home/durst/nba_queries/samples \
     --mount type=bind,source="$(pwd)"/q,target=/home/durst/q \
     --mount type=bind,source="$(pwd)"/data,target=/home/durst/big_dev/nba-movement-data/data \
     --mount type=bind,source=/home/durst/big_dev/fiodar_data/2_6_21,target=/home/durst/big_dev/fiodar_data/2_6_21 \
